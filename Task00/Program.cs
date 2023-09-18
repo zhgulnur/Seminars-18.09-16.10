d30e668fdc0f5274d2ee3a095fcb6ced9665f4c7
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-18 13:51:45 +0600

    Added square commands

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя). 
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Enter the number: ");
+int number = Console.ReadLine();
+Console.Write("Square of the number is ");
+Console.WriteLine(nuintmber*number);
