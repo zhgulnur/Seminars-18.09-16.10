f9a335b0a622e42270cfbd20aeaca37599e0e537
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-20 13:12:48 +0600

    Modified Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -9,15 +9,6 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2)
-{
-    Console.WriteLine("Input Error!");
-}
-else if (number1 > number2) 
-{
-    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number1}");
-}
-else if (number2 > number1) 
-{
-    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number2}");
-}
+int max = number2;
+if (number1 > number2) max = number1;
+Console.WriteLine($"a = {number1}; b = {number2} -> max = {max}");
