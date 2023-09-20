f2ebcb0008efe161030e5f587924bce8d70341e0
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-20 14:22:15 +0600

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter an integer larger than 0: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 2)
+{
+    int count = 2;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
+else if (number>=0 & number<2)
+{
+    Console.WriteLine("There are no even numbers");
+}
+else if (number<0)
+{
+    Console.WriteLine("Input error! The number should be larger than 0.");
+}
