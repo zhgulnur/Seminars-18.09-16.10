18d7ef4fb0482f8946ae8851824e75e5cb78cb8b
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-20 13:19:27 +0600

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} -> yes, it is an even number");
+}
+else
+{
+    Console.WriteLine($"{number} -> no, it is not an even number");
+}
