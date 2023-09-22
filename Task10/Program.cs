decf4812e3b06bbab549f0da66d885ae5fdeeb48
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-22 11:38:05 +0600

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int GetUserInput(string str)
+// (string str) -> потому что мы дальше пишем "Введите число"
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SecondDigit(int EnteredNumber)
+{
+    int TwoDigits = EnteredNumber / 10;
+    int OneDigit = EnteredNumber / 100;
+    int result = TwoDigits - OneDigit * 10;
+    return result;
+}
+
+int number = GetUserInput("Enter a three-digit number: ");
+int secondDigit = SecondDigit(number);
+
+if (number>=100 && number<=999 || number>=-999 && number<=-100) 
+{
+    Console.WriteLine($"{number} -> {secondDigit}");
+}
+else Console.WriteLine("Input error! The number is out of range.");
