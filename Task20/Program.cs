e736dc22d463ac6465876d9b7d3fdfc119af34c0
Гульнур <gulnur.zhakhina@gmail.com>
2023-09-25 14:32:41 +0600

    Seminar 3

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -36,3 +36,28 @@ int y2= Convert.ToInt32(Console.ReadLine());
 double length = Length(x1, x2, y1, y2);
 
 Console.WriteLine($"A ({x1}, {y1}); B ({x2}, {y2}) -> {length}");
+
+
+
+// int GetUserInput(string message)
+// {
+//     Console.Write($"{message}: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     return num;
+// }
+
+// // d = √((хА –  хВ)2 + (уА – уВ)2)
+// double Distance(int x1, int y1, int x2, int y2)
+// {
+//     double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow((y2 - y1), 2));
+//     return res;
+// }
+
+// int x1 = GetUserInput("Координаты первой точки\nВведите координату X");
+// int y1 = GetUserInput("Координаты первой точки\nВведите координату Y");
+// Console.WriteLine("------------------------------");
+// int x2 = GetUserInput("Координаты второй точки\nВведите координату X");
+// int y2 = GetUserInput("Координаты второй точки\nВведите координату Y");
+
+// double distance = Distance(x1, y1, x2, y2);
+// Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
